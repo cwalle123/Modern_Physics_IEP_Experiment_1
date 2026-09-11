31464ba0f55c0a346c955fea344191b6d935075b
cwalle123 <wcliftonjohn@gmail.com>
2026-09-11 09:54:31 +0200

    new correct l3 value

--- compute_g.py
+++ compute_g.py
@@ -19,7 +19,7 @@ d_12 = 7.5                                              # cm  distance between g
 l_2_uncertainty = 0.1                                   # cm
 d_12_uncertainty = l_1_uncertainty + l_2_uncertainty    # cm
  
-l_3 = 0.5                                               # cm
+l_3 = 1.3                                               # cm
 l_3_uncertainty = 0.1                                   # cm
 d_13_uncertainty = l_1_uncertainty + l_3_uncertainty    # cm
  
